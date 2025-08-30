3c274a18707f82bec633eb99993c82e6482751e6
Akshay Reddy <akshaydevelops@gamil.com>
2025-08-30 23:34:01 +0530

    added package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+
+import PackageDescription
+
+let version = "0.0.7"
+let checksum = "25979b2bde8b06a2f868686c75e7c72ba3644c544634512b5ab2388cf8e3ea1f"
+
+let package = Package(
+    name: "Renderer",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "Renderer",
+            targets: ["Renderer"]),
+    ],
+    targets: [
+        .binaryTarget(name: "Renderer",
+                      url: "https://github.com/sureshkumargondi/Renderer-framework/releases/download/\(version)/Renderer.xcframework.zip",
+                      checksum: checksum),
+        .target(
+            name: "Renderer"
+        )
+    ]
+)
